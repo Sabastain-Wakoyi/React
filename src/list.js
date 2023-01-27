e89333538632823c285d77f901c3bfa8241c1f8b
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-01-27 10:08:25 -0600

    render

--- src/list.js
+++ src/list.js
@@ -1,14 +1,56 @@
-import React from "react";
+import React, { setState } from "react";
 
-// The "functional component" syntax lets you use a function
-// as a component. It's effectively just the render method
-function FunctionalComponentSyntax(props) {
-    return (
-        <div>Hello World!</div>
-    );
-}
 
-// Or, use ES6 arrow function syntax to declare the component
-const ArrowFunctionComponent = (props) => (
-    <div>Hello World!</div>
-);
+function List(props) {
+    const allSpeakers = ['Scott Hanselman', 'John Papa', 'Scott Guthrie', 'Dan Wahlin', 'Debora Kurata', 'Zoiner Tejada', 'Scott Allen', 'Elijah Manor', 
+    'Ward Bell', 'Todd Anglin', 'Saron Yitbare', 'Scott Hunter'];
+
+    const [speakers, setSpeakers] = useState(allSpeakers)
+
+    onSortAscendedClicked = () => {
+        const sortedSpeakers = speakers.slice().sort();
+        const reverseSpeakers = sortedSpeakers.slice().reverse();
+        setState(sortedSReversedpeakers)
+
+    }
+        onSortDescendedClicked =() => {
+            const sortedSpeakers = this.state.speakers.slice().sort();
+            setSpeakers(sortedSpeakers)
+        }
+
+        onScottsClicked = () => {
+            const onlyScotts = speakers.filter(name =>
+                name.startsWith("Scott")
+                );
+                setState(onlyScotts);
+
+            }
+
+            onResetClicked = () => {
+                setState(allSpeakers);
+            }
+  const spearkerListItems = speakers.map(speaker =>
+    <li key={speaker}>{speaker}</li>
+    )
+
+
+return(
+    <div className="Container">
+
+   <div className="container">
+    <button className="button" onClick={onSortAscendedClicked}>sortedSReversedpeakers</button>
+    <button className="button" onClick={oonSortDescendedClicked}>sortedSpeakers</button>
+    <button className="button" onClick={onScottsClicked}>Scott</button>
+    <button className="button" onClick={onResetClicked}>Reset</button>
+    
+    </div>
+<div className="container">
+    <ul>{spearkerListItems}</ul>
+
+</div>
+    </div>
+)
+
+    }
+
+    export default List
